3dd616040b3e977e88f17a10a6232e03b4d6d5cf
Andres Cordova <andrescordovaoficial@gmail.com>
2023-07-06 16:50:32 -0600

    Types and Interfaces

--- course-ts-1/typos.ts
+++ course-ts-1/typos.ts
@@ -8,19 +8,46 @@ const saludar = ({ name, age }: { name: string; age: number }) => {
 saludar({ name: "Juan", age: 3 });
 
 const hero = {
-    name: 'Thor',
-    age: 23
-}
+  name: "Thor",
+  age: 23,
+};
 
 type Hero = {
-    readonly id?: number
-    name: string;
-    age: number;
-}
+  readonly id?: number;
+  name: string;
+  age: number;
+};
 
 saludar(hero);
 
+const languages: string[] = [];
+languages.push("JavaScript");
+languages.push("TypeScript");
 
-const languages: string[] = []
-languages.push('JavaScript')
-languages.push('TypeScript')
+interface Heroes {
+  id: number;
+  name: string;
+  age: number;
+}
+
+interface Mario {
+  company: "nintendo";
+  name: string;
+  jump: () => void;
+}
+
+interface Sonic {
+  company: "sega";
+  name: string;
+  run: () => void;
+}
+
+type Personaje = Mario | Sonic;
+
+function jugar(personaje: Personaje) {
+  if (personaje.company === "nintendo") {
+    personaje.jump(); //-> este es mario.
+  } else {
+    personaje.run(); //-> este es sonic
+  }
+}
